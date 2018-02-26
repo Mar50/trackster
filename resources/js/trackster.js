0b008d7daf8459a1e9dd2976c814dedd3e0e88b8
Mar50 <Mar50@users.noreply.github.com>
2018-02-26 14:19:19 +1100

    Finished header, submit query

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,8 +1,22 @@
+$( document ).ready(function() {
+  $("#search-button").click( function(){
+    var $search_text = $("#search_input").val();
+    Trackster.searchTracksByTitle($search_text);
+  });
+
+  $.ajax({
+    url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=hello&api_key=4e0b6eea2b15d8e28397a162c8d6bd9c&format=json",
+    dataType: "jsonp"
+  });
+});
+
+const API_KEY = "4e0b6eea2b15d8e28397a162c8d6bd9c";
+
 var Trackster = {};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
